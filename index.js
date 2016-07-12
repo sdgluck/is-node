ccd257b70b2968c86dddbd2d7e1c8250832d977f
Shawn Inder <shawninder@gmail.com>
2016-07-12 16:49:22 -0400

    Update index.js
    export a boolean instead of a function

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Coding standard for this project defined @ https://github.com/MatthewSH/standards/blob/master/JavaScript.md
 'use strict';
 
-exports = module.export = function isNode () {
-  return !!(typeof process !== 'undefined' && process.versions && process.versions.node);
-};
+exports = module.export = !!(typeof process !== 'undefined' && process.versions && process.versions.node);
